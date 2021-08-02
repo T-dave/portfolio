c493594d09cf2e8bfde8d7a85291624780b99f61
T-dave <54812018+T-dave@users.noreply.github.com>
2021-08-02 16:23:16 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -122,7 +122,7 @@ button[0].addEventListener("click", hireDisplay);
 button[1].addEventListener("click", hireDisplay);
 
 function homeDisplay(){
-	content.innerHTML = "<p style='padding-top: 0;'>Hi...</p><p>My name is David Omotara</p><p>I'm a front end developer with a passion for pushing myself beyond my limts</p><button class='hire button' onclick = 'hireDisplay()'>Contact Me</button>";
+	content.innerHTML = "<p class = 'animated fadeIn' style='padding-top: 0;'>Hi...</p><p class = 'animated fadeIn'>My name is David Omotara</p><p class = 'animated fadeIn'>I'm a front end developer with a passion for pushing myself beyond my limts</p><button class='hire button animated fadeIn' onclick = 'hireDisplay()'>Contact Me</button>";
 	content.classList.remove('skl');
 	home.classList.add('current');
 	services.classList.remove('current');
@@ -132,7 +132,7 @@ function homeDisplay(){
 	console.log('hiiiiiii');
 }
 function serviceDisplay(){
-	content.innerHTML = "<h3>My services include</h3><ul><li>Website Building</li><li>PSI to HTML</li><li>Code Correction/ Cleaning</li><li>Wordpress Development</li><li>Plugin Installation</li><li>HTML to Wordpress</li><li>Custom Wordpress Theme</li></ul><button class='hire button' onclick = 'hireDisplay()'>Contact Me</button>";
+	content.innerHTML = "<h3 class = 'animated fadeIn'>My services include</h3><ul class = 'animated fadeIn'><li>Website Building</li><li>PSI to HTML</li><li>Code Correction/ Cleaning</li><li>Wordpress Development</li><li>Plugin Installation</li><li>HTML to Wordpress</li><li>Custom Wordpress Theme</li></ul><button class='hire button' onclick = 'hireDisplay()'>Contact Me</button>";
 	content.classList.remove('skl');
 	services.classList.add('current');
 	home.classList.remove('current');
@@ -142,7 +142,7 @@ function serviceDisplay(){
 	console.log('hiiiiiii');
 }
 function skillDisplay(){
-	content.innerHTML = "<div id='hexagon' style='font-family: sans-serif;'>HTML</div><div id='hexagon' style='font-family: sans-serif;'>CSS</div><div id='hexagon' style='font-family: sans-serif;'>JAVASCRIPT</div><div id='hexagon' style='font-family: sans-serif;'>BOOTSTRAP</div><div id='hexagon' style='font-family: sans-serif;'>WORDPRESS</div><div id='hexagon' style='font-family: sans-serif;'>REACT</div><button class='hire button' onclick = 'hireDisplay()'>Contact Me</button>";
+	content.innerHTML = "<div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>HTML</div><div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>CSS</div><div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>JAVASCRIPT</div><div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>BOOTSTRAP</div><div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>WORDPRESS</div><div class = 'animated fadeIn' id='hexagon' style='font-family: sans-serif;'>REACT</div><button class='hire button' onclick = 'hireDisplay()'>Contact Me</button>";
 	// content.style.display = "grid";
 	content.classList.add('skl');
 	skills.classList.add('current');
@@ -153,7 +153,7 @@ function skillDisplay(){
 	console.log('hiiiiiii');
 }
 function projectDisplay(){
-	content.innerHTML = "<div class='pics'><a href='project/prj/new.html' target='_blank'><div class='container'><img src='img/prj/01.jpeg'><div class='overlay'><div class='text'>A simple color picker</div></div></div></a><a href='project/primax/index.html' target='_blank'><div class='container'><img src='img/prj/02.jpeg'><div class='overlay'><div class='text'>Primax care</div></div></div></a><a href='project/header/index2.html' target='_blank'><div class='container'><img src='img/prj/03.jpeg'><div class='overlay'><div class='text'>Custom made header</div></div></div></a><a href='project/sona/index.html' target='_blank'><div class='container'><img src='img/prj/04.jpeg'><div class='overlay'><div class='text'>Floufort Consulting service</div></div></div></a><a href='project/scrabbler/index.html' target='_blank'><div class='container'><img src='img/prj/05.jpeg'><div class='overlay'><div class='text'>Word Scrabbler</div></div></div></a><a href='project/portfolio/portfolio.html' target='_blank'><div class='container'><img src='img/prj/06.jpeg'><div class='overlay'><div class='text'>Custom made template</div></div></div></a></div>"
+	content.innerHTML = "<div class='pics animated fadeIn'><a href='project/prj/new.html' target='_blank'><div class='container'><img src='img/prj/01.jpeg'><div class='overlay'><div class='text'>A simple color picker</div></div></div></a><a href='project/primax/index.html' target='_blank'><div class='container'><img src='img/prj/02.jpeg'><div class='overlay'><div class='text'>Primax care</div></div></div></a><a href='project/header/index2.html' target='_blank'><div class='container'><img src='img/prj/03.jpeg'><div class='overlay'><div class='text'>Custom made header</div></div></div></a><a href='project/sona/index.html' target='_blank'><div class='container'><img src='img/prj/04.jpeg'><div class='overlay'><div class='text'>Floufort Consulting service</div></div></div></a><a href='project/scrabbler/index.html' target='_blank'><div class='container'><img src='img/prj/05.jpeg'><div class='overlay'><div class='text'>Word Scrabbler</div></div></div></a><a href='project/portfolio/portfolio.html' target='_blank'><div class='container'><img src='img/prj/06.jpeg'><div class='overlay'><div class='text'>Custom made template</div></div></div></a></div>"
 	content.classList.remove('skl');
 	projects.classList.add('current');
 	home.classList.remove('current');
@@ -163,7 +163,7 @@ function projectDisplay(){
 	console.log('hiiiiiii');
 }
 function hireDisplay(){
-	content.innerHTML = "<div class='form'><h2>Get in Touch</h2><form><div class='name-email'><div class='flex-form'>Name<br><input type='text' name='name' placeholder='Name'></div><div>Email<br><input type='text' name='email' placeholder='email'></div></div><div class='textarea'>Message<br><textarea>Message</textarea></div></form></div><button style='width: auto;' class='hire'>Send Message</button>";
+	content.innerHTML = "<div class='form animated fadeIn'><h2>Get in Touch</h2><form action='mailto:tomyomo@gmail.com' method='POST' enctype='multipart/form-data' name='EmailForm'><div class='name-email'><div class='flex-form'>Name<br><input type='text' name='name' placeholder='Name'></div><div class='flex-forma'>Email<br><input type='text' name='email' placeholder='email'></div></div><div class='textarea'>Message<br><textarea>Message</textarea></div><input type='submit' value='Send Message' class='hireh'></form></div>";
 	content.classList.remove('skl');
 	hire.classList.add('current');
 	home.classList.remove('current');
